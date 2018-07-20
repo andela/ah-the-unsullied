ebaaf85c1880a4a247ffeab3e1e57ec35173caaf
Ephraim Malinga <ephraim.malinga@gmail.com>
2018-07-20 09:50:12 +0300

    Revert "#159135657 Integrate TravisCI with readme badge (#1)" (#2)
    This reverts commit 23890273be5910ce4cecb843384e69c188e6570f.

--- authors/apps/authentication/urls.py
+++ authors/apps/authentication/urls.py
@@ -1,20 +1,11 @@
 from django.conf.urls import url
-from django.urls import path
 
 from .views import (
     LoginAPIView, RegistrationAPIView, UserRetrieveUpdateAPIView
 )
 
-# urlpatterns = [
-#     url(r'^user/?$', UserRetrieveUpdateAPIView.as_view()),
-#     url(r'^users/?$', RegistrationAPIView.as_view()),
-#     url(r'^users/login/?$', LoginAPIView.as_view()),
-# ]
-
-
-# Use the new path module in django 2.0
 urlpatterns = [
-    path('user/', UserRetrieveUpdateAPIView.as_view()),
-    path('users/', RegistrationAPIView.as_view()),
-    path('users/login/', LoginAPIView.as_view()),
+    url(r'^user/?$', UserRetrieveUpdateAPIView.as_view()),
+    url(r'^users/?$', RegistrationAPIView.as_view()),
+    url(r'^users/login/?$', LoginAPIView.as_view()),
 ]
--- authors/settings.py
+++ authors/settings.py
@@ -36,7 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    # 'corsheaders',
+    'corsheaders',
     'django_extensions',
     'rest_framework',
 
@@ -48,8 +48,8 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    # 'corsheaders.middleware.CorsMiddleware',
-    # 'django.middleware.common.CommonMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
@@ -134,24 +134,10 @@ CORS_ORIGIN_WHITELIST = (
 AUTH_USER_MODEL = 'authentication.User'
 
 REST_FRAMEWORK = {
-    # 'EXCEPTION_HANDLER': 'authors.apps.core.exceptions.core_exception_handler',
-    # 'NON_FIELD_ERRORS_KEY': 'error',
+    'EXCEPTION_HANDLER': 'authors.apps.core.exceptions.core_exception_handler',
+    'NON_FIELD_ERRORS_KEY': 'error',
 
-    # # Added the default permisison classes - Ephraim
-    # # 'DEFAULT_PERMISSION_CLASSES':(
-    # #     'rest_framework.persmissions.IsAuthenticated',
-    # # ),
-
-    # 'DEFAULT_AUTHENTICATION_CLASSES': (
-    #     'authors.apps.authentication.backends.JWTAuthentication',
-    # ),
-
-    'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.IsAuthenticated',
-    ),
-
-    'DEFAULT_AUTHENTICATED_CLASSES':(
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'authors.apps.authentication.backends.JWTAuthentication',
     ),
 }
