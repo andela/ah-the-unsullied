95bdf0e236fa74c637fa5e0c7fb6320c8a6c54e9
kevin munene mutahi <nesh@kevins-MacBook-Pro.local>
2019-01-07 17:25:37 +0300

    chore(upgrade): upgrade django version
    - Updated django version from 1.1 to 2.1 as well as requirements
    packages
    - updated url files to match django version 2.1 conventions
    - remove whitespaces
    - remove commented line
    
    [starts #162949198]

--- authors/apps/authentication/urls.py
+++ authors/apps/authentication/urls.py
@@ -1,11 +1,14 @@
-from django.conf.urls import url
 
+from django.urls import path
 from .views import (
     LoginAPIView, RegistrationAPIView, UserRetrieveUpdateAPIView
 )
 
+""" Django 2.0 requires the app_name variable set when using include namespace"""
+app_name = 'authentication'
+
 urlpatterns = [
-    url(r'^user/?$', UserRetrieveUpdateAPIView.as_view()),
-    url(r'^users/?$', RegistrationAPIView.as_view()),
-    url(r'^users/login/?$', LoginAPIView.as_view()),
+    path('user/', UserRetrieveUpdateAPIView.as_view()),
+    path('users/', RegistrationAPIView.as_view()),
+    path('users/login/', LoginAPIView.as_view()),
 ]
--- authors/settings.py
+++ authors/settings.py
@@ -138,6 +138,6 @@ REST_FRAMEWORK = {
     'NON_FIELD_ERRORS_KEY': 'error',
 
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'authors.apps.authentication.backends.JWTAuthentication',
+        # 'authors.apps.authentication.backends.JWTAuthentication',
     ),
 }
